955eee864de6e3ce2a205d6bd243b7e3892c916e
Cnidarias <roessner.pascal@gmail.com>
2025-05-22 11:05:58 +0200

    🎉 Pivick: Start Clickhouse Pivot Explorer Project

---
+++ src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from "@angular/core";
+import { PivickLayoutComponent } from "./pivick-layout/pivick-layout";
+import { Config } from "./services/config";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.html",
+  styleUrls: ["./app.scss"],
+  standalone: true,
+  imports: [PivickLayoutComponent],
+})
+export class AppComponent {
+  private config: Config = inject(Config);
+  constructor() {
+    this.config.locale$.subscribe((locale) => {
+      console.log("Using locale", locale);
+    });
+  }
+}

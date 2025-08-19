254c582ed9b52562453ba4fc1a8054774b128eb4
Cnidarias <roessner.pascal@gmail.com>
2025-08-19 13:19:42 +0200

    ✨ Selection: Allow removing single items from the report

---
+++ src/app/components/selection-list/selection-list-box/selection-list-box.ts
@@ -0,0 +1,94 @@
+import { Component, inject, Input, input, OnInit } from "@angular/core";
+import { Button } from "primeng/button";
+import { Listbox } from "primeng/listbox";
+import { TranslatePipe } from "@ngx-translate/core";
+import { PivickAnalysis } from "../../../services/pivick-analysis";
+import { TCubeDimension, TCubeMeasure } from "@cubejs-client/core";
+import { Observable } from "rxjs";
+
+@Component({
+  selector: "app-selection-list-box",
+  imports: [Button, Listbox, TranslatePipe],
+  templateUrl: "./selection-list-box.html",
+  styleUrl: "./selection-list-box.scss",
+})
+export class SelectionListBox implements OnInit {
+  @Input({ required: true }) title!: string;
+  @Input({ required: true }) emptyMessage!: string;
+  @Input({ required: true }) type!: "dimension" | "measure";
+  @Input({ required: true }) target!: "rows" | "columns" | "measures";
+  @Input({ required: true }) itemObservable!: Observable<string[]>;
+
+  protected pivickAnalysis = inject(PivickAnalysis);
+
+  items: (TCubeDimension | TCubeMeasure)[] = [];
+
+  ngOnInit() {
+    this.itemObservable.subscribe((elementKeys) => {
+      this.items = elementKeys.map((key) => {
+        const item = this.pivickAnalysis.getDimensionOrMeasureByKey(key);
+        return item
+          ? item
+          : ({ name: key, shortTitle: key } as TCubeDimension | TCubeMeasure);
+      });
+    });
+  }
+
+  onDragOver($event: DragEvent) {
+    if (!$event.dataTransfer) {
+      return;
+    }
+    const searchKey =
+      this.type === "dimension"
+        ? "pivick/dimension-node"
+        : "pivick/measure-node";
+    if ($event.dataTransfer.types.includes(searchKey)) {
+      $event.preventDefault();
+      $event.dataTransfer.dropEffect = "copy";
+    } else {
+      $event.dataTransfer.dropEffect = "none";
+    }
+  }
+
+  onTargetDrop($event: DragEvent) {
+    if (!$event.dataTransfer) {
+      return;
+    }
+    const searchKey =
+      this.type === "dimension"
+        ? "pivick/dimension-node"
+        : "pivick/measure-node";
+    const key = $event.dataTransfer.getData(searchKey);
+
+    if (!key) {
+      $event.dataTransfer.dropEffect = "none"; // Show no drop cursor
+      return;
+    }
+
+    $event.preventDefault();
+
+    if (this.type === "dimension") {
+      if (this.target === "rows") {
+        this.pivickAnalysis.addRow(key);
+      } else if (this.target === "columns") {
+        this.pivickAnalysis.addColumn(key);
+      }
+    } else {
+      this.pivickAnalysis.addMeasure(key);
+    }
+  }
+
+  onRemoveFromSelection(item: TCubeDimension | TCubeMeasure) {
+    switch (this.target) {
+      case "rows":
+        this.pivickAnalysis.removeRow(item.name);
+        break;
+      case "columns":
+        this.pivickAnalysis.removeColumn(item.name);
+        break;
+      case "measures":
+        this.pivickAnalysis.removeMeasure(item.name);
+        break;
+    }
+  }
+}

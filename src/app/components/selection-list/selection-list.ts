c9f79db6ae6f857ddd5837e67af00ceaee557e43
Cnidarias <roessner.pascal@gmail.com>
2025-08-25 14:43:41 +0200

    ✨ SelectionList: Add navbar for settings buttons

--- src/app/components/selection-list/selection-list.ts
+++ src/app/components/selection-list/selection-list.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { SelectionListBox } from './selection-list-box/selection-list-box';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroTrash } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-selection-list',
-  imports: [],
+  imports: [SelectionListBox, NgIcon],
+  providers: [provideIcons({ heroTrash })],
   templateUrl: './selection-list.html',
   styleUrl: './selection-list.css',
 })
